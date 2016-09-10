41a14faaeb7e8f6a4e83a06922620a12643a2b30
pavan52p <p052pavan@gmail.com>
2016-09-10 16:01:42 +0530

    10/sep-4:01

---
+++ NewProjectForJenkins/src/a/A.java
@@ -0,0 +1,24 @@
+package a;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class A {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String rootPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver",rootPath+"/TestData/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();		
+
+	}*/
+	
+	@Test
+	public void browserTest(){
+			String rootPath = System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver",rootPath+"/TestData/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();		
+	}
+
+}
